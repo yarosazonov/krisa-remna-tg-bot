77d6f42bcf1170a94f41ce88d837183d81baec17
Yaro <sazonovfx@gmail.com>
2026-01-14 11:23:43 +0300

    fix: remove cli inports from __init__ to prevent circular import warning

--- utils/backup/__init__.py
+++ utils/backup/__init__.py
@@ -3,5 +3,4 @@
 from .postgres import backup_postgres, restore_postgres
 from .sqlite import backup_sqlite, restore_sqlite
 from .archive import create_encrypted_archive, decrypt_archive
-from .cli import main, run_backup, run_restore
 
