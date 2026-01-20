a4993696e4678e95b515ea2baab648aa09190fff
Yaro <sazonovfx@gmail.com>
2026-01-20 17:18:46 +0300

    refactor: remove backup logic (moved to the ops repo)

--- utils/backup/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Backup and restore utilities for Remna PostgreSQL and Krisa-Remna-TG-Bot SQLite databases."""
-
-from .postgres import backup_postgres, restore_postgres
-from .sqlite import backup_sqlite, restore_sqlite
-from .archive import create_encrypted_archive, decrypt_archive
-from .cleanup import cleanup_old_backups
-
